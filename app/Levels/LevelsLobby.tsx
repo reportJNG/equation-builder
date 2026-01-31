43607ce855fb79e2749a39e4f9b8ed41feb8eb5c
reportJNG <hamzaremali10@gmail.com>
2026-01-31 12:40:00 -0800

    created for user levels lobby

---
+++ app/Levels/LevelsLobby.tsx
@@ -0,0 +1,55 @@
+import styles from "./LevelLobby.module.css";
+import { Settings, SkipBackIcon, User } from "lucide-react";
+
+interface LevelsLobbyprops {
+  name: string;
+}
+export default function LevelsLobby({ name }: LevelsLobbyprops) {
+  const confirmeQuit = () => {};
+  const settingsopen = () => {};
+  return (
+    <div className={styles.container}>
+      <div className={styles.topbar}>
+        <div className={styles.left}>
+          <div className={styles.hanlderquit}>
+            <button
+              className={styles.quitbutton}
+              aria-label="Quit"
+              title="Quit"
+              onClick={confirmeQuit}
+            >
+              <SkipBackIcon />
+            </button>
+          </div>
+        </div>
+        <div className={styles.mid}>
+          <div className={styles.textmidscreen}>
+            <h2>Brain Equation</h2>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.profilehandler}>
+            <div className={styles.usericon}>
+              <User />
+            </div>
+            <div className={styles.nameplayer}>{name}</div>
+          </div>
+          <div className={styles.settingshandler}>
+            <div className={styles.settingicon}>
+              <span
+                aria-label="Settings"
+                title="Settings"
+                onClick={settingsopen}
+              >
+                <Settings />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.body}>
+        {/**call levels component show user in lobby this coponnent*/}
+      </div>
+    </div>
+  );
+}
