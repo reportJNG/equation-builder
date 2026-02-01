4686b20ffcca8a4cc848e91101391b876e42e1d0
reportJNG <hamzaremali10@gmail.com>
2026-02-01 00:17:31 -0800

    created levels lobby .tsx basic

---
+++ app/Levels/Levels.tsx
@@ -0,0 +1,9 @@
+import styles from "./Levels.module.css";
+
+export default function Levels() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.boxhandlerlevel}></div>
+    </div>
+  );
+}
