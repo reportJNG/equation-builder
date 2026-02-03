6364458d326cf8afb17e7bca11c215113efa4622
reportJNG <hamzaremali10@gmail.com>
2026-02-02 23:49:16 -0800

    fixed build to deploy on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
-import { i18n } from "./next-i18next.config";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // optional but recommended
-  i18n, // add your i18n config here
 };
 
 export default nextConfig;
